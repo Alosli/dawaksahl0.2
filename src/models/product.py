4e908606e41de7d6ecd0de3bf67cb980135f801d
Alosli <Aloslimaho@gmail.com>
2025-08-08 20:45:35 +0300

    delete fda

--- src/models/product.py
+++ src/models/product.py
@@ -116,15 +116,7 @@ class Product(db.Model):
     is_otc = db.Column(db.Boolean, default=True)  # Over-the-counter
     is_generic = db.Column(db.Boolean, default=False)
     is_available = db.Column(db.Boolean, default=True)
-    
-    # FDA Information
-    fda_application_number = db.Column(String(50), nullable=True)
-    fda_approved = db.Column(Boolean, default=False)
-    approval_date = db.Column(String(50), nullable=True)
-    marketing_status = db.Column(String(100), nullable=True)
-    therapeutic_class = db.Column(String(255), nullable=True)
-    active_ingredients = db.Column(JSON, nullable=True)  # Array of {name, strength}
-    
+        
     # Quality and Ratings
     rating = db.Column(db.Float, default=0.0)
     total_reviews = db.Column(db.Integer, default=0)
